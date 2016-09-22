ddc25c0e25f5ad5ede9f78bc04c1951865422653
Sasha Cooper <jinksy@gmail.com>
2016-09-22 15:59:14 +0100

    Buildin' a model

--- crockerometer/views.py
+++ crockerometer/views.py
@@ -3,6 +3,7 @@ from django.shortcuts import render
 # Create your views here.
 def index(request):
   value = 'look at mah value'
-  context = { 'interpolation_test': value }
+  users = {'Bob the user', 'Quentin the other user'}
+  context = { 'interpolation_test': value, 'users': users }
 
   return render(request, 'index.html', context)
