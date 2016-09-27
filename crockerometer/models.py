79b5ca30c70e151145198f3667638ef0b3e6e894
Sasha Cooper <jinksy@gmail.com>
2016-09-27 16:04:55 +0100

    Add Vote and Metric models

--- crockerometer/models.py
+++ crockerometer/models.py
@@ -1,10 +1,18 @@
 from django.db import models
+from django.contrib.auth.models import User
+from django.core.validators import MinValueValidator, MaxValueValidator
 
 # Create your models here.
 
-class User(models.Model):
-  name = models.CharField(max_length=255)
-  email = models.CharField(max_length=255)
+class Metric(models.Model):
+  users = models.ManyToManyField(User, through = 'Vote')
+  name = models.CharField(max_length = 255)
+
+class Vote(models.Model):
+  metric = models.ForeignKey(Metric, on_delete = models.CASCADE)
+  user = models.ForeignKey(Person, on_delete = models.CASCADE)
+  rating = models.IntegerField(validators = [MinValueValidator(0), MaxValueValidator(10)])
+  email = models.EmailField
 
   def __str__(self):
-    return self.name
+    return str(self.rating)
