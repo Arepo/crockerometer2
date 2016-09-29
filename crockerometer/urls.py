875f94ac4b386c8283e3230134f8a02143c28f33
Sasha Cooper <jinksy@gmail.com>
2016-09-29 12:15:42 +0100

    Move boilerplate index code to base file

--- crockerometer/urls.py
+++ crockerometer/urls.py
@@ -2,6 +2,6 @@ from django.conf.urls import url
 from . import views
 
 urlpatterns = [
-  url(r'^/([0-9]+)/$', views.detail, name='detail'),
+  url(r'^([0-9]+)$', views.detail, name='detail'),
   url(r'^$', views.index)
 ]
